908f1e5f294ff368b8478b1e5a67add43630f1a4
ilyesnb <ilyesnabi9@gmail.com>
2022-01-21 21:59:38 +0100

    init

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import myvideo from "./myvideo.mp4"
 
 function App() {
   return (
-    <div>
+    <>
       <div style={{ border: "solid 1px black", maxWidth: "100w" }}>
         <h1 className="titleRed">votre nom ici</h1>
         <br /> 
@@ -17,7 +17,7 @@ function App() {
       <video width="320" height="240" controls>
         <source src={myvideo} type="video/mp4" />
       </video>
-    </div>
+    </>
   );
 }
 export default App;
